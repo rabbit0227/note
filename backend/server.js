72fb084df86207441dad2a84593b3cbc255fb1f9
Zer0-and-0ne <zer0and0ne.0001@gmail.com>
2024-08-02 23:36:54 -0400

    a

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,6 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Import the Note model
-const Note = require("./models/note");
-
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
@@ -20,52 +17,39 @@ mongoose.connect(process.env.MONGODB_URI, {
   useUnifiedTopology: true,
 });
 
+// Note Schema
+const noteSchema = new mongoose.Schema({
+  title: String,
+  text: String,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
 // Routes
-
-// Get all note
 app.get("/note", async (req, res) => {
-  try {
-    const note = await Note.find();
-    res.json(note);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  const notes = await Note.find();
+  res.json(notes);
 });
 
-// Create a new note
 app.post("/note", async (req, res) => {
-  try {
-    const newNote = new Note({
-      title: req.body.title,
-      text: req.body.text,
-    });
-    await newNote.save();
-    res.status(201).json(newNote);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  const newNote = new Note({
+    title: req.body.title,
+    text: req.body.text,
+  });
+  await newNote.save();
+  res.status(201).json(newNote);
 });
 
-// Update a note
-app.put("/note/:id", async (req, res) => {
-  try {
-    const updatedNote = await Note.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    res.json(updatedNote);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// Delete a note
 app.delete("/note/:id", async (req, res) => {
-  try {
-    await Note.findByIdAndDelete(req.params.id);
-    res.status(204).send();
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  await Note.findByIdAndDelete(req.params.id);
+  res.status(204).send();
+});
+
+app.put("/note/:id", async (req, res) => {
+  const updatedNote = await Note.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.json(updatedNote);
 });
 
 // Start Server
